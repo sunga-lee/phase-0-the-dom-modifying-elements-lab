4365751a04ff74322aeb7bf0fd37263ff0f4b989
sunga-lee <sungajunior1@gmail.com>
2023-03-24 21:23:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const body = document.querySelector("body");
+const main = document.querySelector("main#main");
+body.removeChild(main);
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "OCHOLLA is the champion"
+body.append(newHeader);
+
 // Write your code here!
